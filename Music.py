8c99393273956e04e79add61af8ed690bed2a486
RsslM <rsslbnlhymrc@gmail.com>
2024-11-18 08:40:41 -0600

    musictrack

--- Music.py
+++ Music.py
@@ -1,54 +1,42 @@
 import csv
+import random
+from datetime import datetime
+
 class Music:
     """Receives information of a song, manage information"""
     
-    def __init__(self, title, artist, duration,album=None):
-        self.title=title
-        self.artist=artist
-        self.album=album
-        self.duration=duration
-
-    def getTitle(self):
-        return self.title
-    
-    def getArtist(self):
-        return self.artist
-    
-    def getAlbum(self):
-        if self.album == None:
-            return "Unknown"
-        return self.album
-    
-    def getTitle(self):
-        return self.title
-    
-    def getDuration(self):
-        return self.duration
-    
-    def convertTime(self):
-        time=self.duration
-        minutes, seconds = map(int, time.split(":"))
-        total_seconds = minutes * 60 + seconds
-        return total_seconds
+    def __init__(self, title, artist, album, duration):
+        self.title = title
+        self.artist = artist
+        self.album = album
+        self.duration = duration 
         
-    def getSongDetails(self, title):
-        """Returns the details of the song in the format
-        Title:
-        Artis:
-        Album:
-        Duration:
-        Arguments: Title of the song"""
-        artist=title
-        with open('Library.csv', 'r') as storage:
-            read=csv.reader(storage)
-            for lines in read:
-                # print(lines[1])
-                if artist in lines[0]:
-                    return(f"Title: {lines[0]}\nArtist: {lines[1]}\nAlbum: {lines[2]}\nDuration: {lines[3]}")
-                else:
-                    return "Song does not Exist"
-    
     def __str__(self):
-        return f"Title: {self.getTitle()}\nArtist: {self.getArtist()}\nAlbum: {self.getAlbum()}\nDuration: {self.getDuration()}"
+        return f"{self.title} by {self.artist} (Album: {self.album})"
     
+    
+class PlayList:
+    
+    def __init__(self, size: int = 100):
+        """Initialize playlist with a max size"""
+        self.storage0 = [None] * size
+        self.size = 0
 
+    def increaseSize(self):
+        # Increase the size of the playlist if needed
+        if self.size >= len(self.storage0):
+            self.storage0.extend([None] * len(self.storage0))
+        self.size += 1
+
+    def getSize(self):
+        """Returns the current size of the playlist"""
+        return self.size
+    
+    def addtoPlaylist(self, song: Music):
+        """Add a Music object to the playlist"""
+        if self.size < len(self.storage0):
+            self.storage0[self.size] = song
+        else:
+            self.increaseSize()
+            self.storage0[self.size - 1] = song
+        self.size += 1
