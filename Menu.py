010d41613890e0c1bb9433e8decd5b5ab957eb15
Gabriel2643 <gv.pacaldo@gmail.com>
2024-11-29 21:38:40 +0800

    updated menu

--- Menu.py
+++ Menu.py
@@ -7,6 +7,7 @@ import LibraryManager
 main={
     1:"Playlists",
     2:"Music Library",
+    3:"View Queue",
     0:"Quit"
 }
 # menu1={
@@ -28,9 +29,16 @@ commands={
     0:"Return"
 }
 library={
-    1:"Play Library",
-    2:"Add Tracks to Library",
+    1:"Play All",
+    2:"Choose Track to Play",
+    3:"Add Tracks to Library",
     0:"Return"
+}
+
+library1={
+    1:"Choose Track",
+    2:"Play all",
+    3:"Return"
 }
 
 def printmenu(menu):
@@ -44,12 +52,6 @@ line1 = "<---Welcome to Python Music Player--->"
 
 if __name__ == "__main__":
     manager=LibraryManager
-<<<<<<< HEAD
-    # print(manager.Library)
-    # for i in manager.Library:
-    #     print(i)
-    printmenu()
-=======
     while True:
         printmenu(main)
         choice1 = input("Enter Choice: ")
@@ -71,31 +73,38 @@ if __name__ == "__main__":
                 printmenu(library)
                 choicelib = input("Enter Choice: ")
                 if choicelib == "1":
-                    """Play Library"""
-                    while True:
-                        printmenu(commands)
-                        choicelib1 = input("Enter Choice")
-                        if choicelib1 == "1":
-                            """Next track"""
-                        elif choicelib1 == "2":
-                            """Previous"""
-                        elif choicelib1 == "3":
-                            """Turn ON Repeat"""
-                        elif choicelib1 == "4":
-                            """Turn OFF Repeat"""
-                        elif choicelib1 == "5":
-                            """Shuffle Queue"""
-                        elif choicelib1 == "6":
-                            """Clear Queue"""
-                        elif choicelib1 == "0":
-                            break
-
+                    """Play All"""
+                    break
                 elif choicelib == "2":
-                    """Add Tracks"""
+                    """Choose Track to Play"""
+                    while True:
+                        manager.showLibrary()
+                        choicelib2 = input("Enter Choice: ")
+                        """Play chosen Track"""
+                        break
                 elif choicelib == "0":
+                    break
+        elif choice1 == "3":
+            """Manage Queue"""
+            """Show Queue"""
+            while True:
+                printmenu(commands)
+                choicelib1 = input("Enter Choice: ")
+                if choicelib1 == "1":
+                    """Next track"""
+                elif choicelib1 == "2":
+                    """Previous"""
+                elif choicelib1 == "3":
+                    """Turn ON Repeat"""
+                elif choicelib1 == "4":
+                    """Turn OFF Repeat"""
+                elif choicelib1 == "5":
+                    """Shuffle Queue"""
+                elif choicelib1 == "6":
+                    """Clear Queue"""
+                elif choicelib1 == "0":
                     break
         elif choice1 == "0":
             exit
 
 
->>>>>>> 2c1a5e3a923524fdc49375af2b38d2218f2921ee
